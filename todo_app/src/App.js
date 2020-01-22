3ee089a77c33a830563352a0deb131bf7b4afbc0
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-21 18:38:26 -0600

    MVP is done works as expected only a small bug where it creates new empty todos on toggle completed

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,13 +1,33 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {todoReducer, initalState} from './reducers/todorReducer';
+import Todo from './components/Todo';
 
 function App() 
 {
   const [state, dispatch] = useReducer(todoReducer, initalState);
+  const [newTodoItem, setNewTodoItem] = useState('');
+  
+  const handleChange = e =>
+  {
+    setNewTodoItem(e.target.value)
+  }
+
   return (
     <div className="App">
-      
+     <h2 className='Title'>Welcome to your Todo App</h2>
+      {state.map(item =>{
+        return <Todo key={item.id} item={item} dispatch={dispatch}/>
+      })}
+
+      <div>
+        <input type='text' name='newTodoItem' value={newTodoItem} onChange={handleChange}/>
+        <button onClick={() => {
+          dispatch({type: 'ADD_TODO', payload: newTodoItem})
+          setNewTodoItem('')}}>Add Item</button>
+
+          <button onClick={() => dispatch({type: 'CLEAR_COMPLETED'})}>Clear Completed Tasks</button>
+      </div>
     </div>
   );
 }
--- todo_app/src/reducers/todorReducer.js
+++ todo_app/src/reducers/todorReducer.js
@@ -1,10 +1,33 @@
-export const todoReducer = (state, action) =>
-{
-    return state;
+export const todoReducer = (state, action) => {
+    switch (action.type) 
+    {
+        case 'ADD_TODO':
+            return [...state, {item: action.payload, completed: false, id: Date.now()}];
+
+        case 'TOGGLE_COMPLETED':
+          const newState = state.map(item => {
+               if(item.id === action.payload)
+               {
+                   item.completed = !item.completed;
+                   return item;
+               }
+               else{
+                   return item;
+               }
+           });
+           return [...state, newState];
+
+           case 'CLEAR_COMPLETED':
+               const tempState = state.filter(item => !item.completed)
+               return [...tempState];
+        default:
+            return state;
+    }
 }
 
-export const initalState = {
-    item: 'Learn about reducers',
-    completed: false,
-    id: Date.now()
-};
+export const initalState = [ 
+    { 
+        item: 'Learn about reducers',
+        completed: false,
+        id: Date.now() 
+    }]
